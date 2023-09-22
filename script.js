b404c3c4b4e90a6a1238b22905dd414a18d12e41
felix11736 <felixzebaoth@gmail.com>
2023-09-22 12:37:58 +0700

    baru

--- script.js
+++ script.js
@@ -20,8 +20,6 @@ window.onclick( (event) => {
 
 // carousel//t
 const carousel = document.querySelector(".carousel");
-
-
 const prevBtn = document.getElementById("prevBtn");
 const nextBtn = document.getElementById("nextBtn");
 let currentIndex = 0;
@@ -48,4 +46,75 @@ nextBtn.addEventListener("click", () => {
 });
 
 showSlide(currentIndex);
+//corousel //
+
+
+//array produk//
+const products = [
+  {
+    id: 1,
+    name: "Laptop ASUS VivoBook 14 OLED",
+    price: "Rp 5.999.000",
+    description: "Ditenagai prosessor Intel Core i5-1135G7 Dilengkapi RAM 8GB dan SSD 512GB Didukung VGA NVIDIA GeForce MX350",
+    link: "https://www.asus.com/id/laptops/for-home/vivobook/",
+    image: "assets/images/1.png",
+  },
+  {
+    id: 2,
+    name: "Laptop ProArt Studiobook 16 3D OLED",
+    price: "Harga ASUS Rp9.999.000",
+    description: "OLED (H7600, 12th Gen Intel)Windows 11 Home - ASUS recommends Windows 11 Pro for businessUp to Intel® Core™ i9-12900H",
+    link: "https://www.asus.com/id/laptops/for-home/vivobook/",
+    image: "assets/images/2.png",
+  },
+  {
+    id: 3,
+    name: "LAPTOP HP GAMING VICTUS 16,1",
+    price: "Harga ASUS Rp19.999.000",
+    description: "SPESIFIKASI Hingga Intel® Core™ i7-12700H hingga 4,7 GHz dengan Intel® Turbo Boost Technology(2g)",
+    link: "https://www.asus.com/id/laptops/for-home/vivobook/",
+    image: "assets/images/victus new.png",
+  },
+  {
+    id: 4,
+    name: "Laptop ASUS VivoBook 14 OLED",
+    price: "Rp 5.999.000",
+    description: "Ditenagai prosessor Intel Core i5-1135G7 Dilengkapi RAM 8GB dan SSD 512GB Didukung VGA NVIDIA GeForce MX350",
+    link: "https://www.asus.com/id/laptops/for-home/vivobook/",
+    image: "assets/images/1.png",
+  },
+  // Tambahkan produk lain di sini
+];
+
+// Array produk
+const produkContainer = document.getElementById("produk-container");
+
+function renderProducts() {
+  produkContainer.innerHTML = ""; // Hapus konten sebelumnya
+
+  products.forEach((product) => {
+    const productCard = document.createElement("div");
+    productCard.classList.add("product");
+
+    productCard.innerHTML = `
+            <img src="${product.image}" alt="${product.name}" />
+            <h3>${product.name}</h3>
+            <p>${product.price}</p>
+            <p>${product.description}</p>
+            <a href="${product.link}" target="_blank">selengkapnya</a>
+        `;
+
+    produkContainer.appendChild(productCard);
+  });
+}
+
+// Panggil fungsi renderProducts untuk pertama kali
+renderProducts();
+
+//animasi mouse//
+document.annousemove =animatedCircles;
+var color 
+//tutup animasi mouse//
+     
+
 
